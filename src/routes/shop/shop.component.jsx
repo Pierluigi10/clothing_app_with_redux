d5799b5d25d7106cdf3f53da69cd72932af810bc
pierluigi <pb@evergreen.de>
2022-10-25 11:11:45 +0200

    rename categories store elements

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategoriesMap } from "../../store/categories/category.action";
+import { setCategoriesMap } from "../../store/categories/categories.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
